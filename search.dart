12e9ae525b400bf6a5817757e13a68f7a65c51c7
egideon1 <evangideon0@gmail.com>
2025-10-07 14:36:35 -0400

    Added Files main.dart, search.dart, and autherization.dart
    These files are incomplete, but add a basis for the UI and backend authorization. All files first commit to main branch, edits and updates should have their own branch to stay consistent. Merge branch only when the update/edit is working.

---
+++ search.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'autherization.dart';
+
+class SearchBox extends StatefulWidget {
+  @override
+  _SearchBoxState createState() => _SearchBoxState();
+}
+
+class _SearchBoxState extends State<SearchBox> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: 'Search for Songs, Artists, or Albums',
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        ),
+        onChanged: (query) {
+          // Search Logic (Call Spotify API)
+          print('Search query: $query');
+        },
+        onSubmitted: (query) {
+          // Trigger Search when user presses enter
+          print('Submitted search query: $query');
+        },
+      ),
+    );
+  }
+
+  // After Authentication
+  Future<Map<String, dynamic>> searchSpotify(String query) async {
+    final String accessToken =
+        getAccessToken() as String; // Replace with authentication key
+    final response = await http.get(
+      Uri.parse(
+        'https://api.spotify.com/v1/search?q=$query&type=track,artist,album',
+      ),
+      headers: {'Authorization': 'Bearer $accessToken'},
+    );
+
+    if (response.statusCode == 200) {
+      // Process search results from JSON response
+      return json.decode(response.body);
+    } else {
+      print('Error searching Spotify: ${response.statusCode}');
+    }
+  }
+}
